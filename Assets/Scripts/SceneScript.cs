5bea5258427c21fc2d3d3091561652a533151d4e
Casimir Kuusela <crk.kuusela@gmail.com>
2018-01-23 21:40:52 +0200

    Some stuff

---
+++ Assets/Scripts/SceneScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneScript : MonoBehaviour
+{
+
+    // Use this for initialization
+    public int level1 = 3;
+    public int menu = 0;
+
+    public int options = 1;
+    public int scoreScreen = 2;
+    void Awake()
+    {
+
+
+		DontDestroyOnLoad(transform.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+
+    public void PlayButtonPress()
+    {
+        SceneManager.LoadScene(level1);
+    }
+
+	public void OptionsButtonPress()
+	{
+		SceneManager.LoadScene(options);
+	}
+
+}
